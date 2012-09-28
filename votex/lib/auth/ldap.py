26bd94587540c358abf06ae6d9a65c9f2ffe0fc9
Maurizio Minelli <m.minelli@e-biz.lu>
2012-09-28 09:58:05 +0200

    changed auth to __auth__ to reflect changes in the baseauth class

--- votex/lib/auth/ldap.py
+++ votex/lib/auth/ldap.py
@@ -3,20 +3,21 @@ from votex.lib.ldapConnector import LdapConnector
 
 
 class LDAPAuthBackend(AbstractAuthBackend):
+  """Authenticate the user against LDAP.
+
+
+  Settings:
+
+  auth.module = ldap
+  auth.class = LDAPAuthBackend
+  ldap.server = ldap://localhost
+  ldap.basedn_users = ou=People,dc=local,dc=org
+  """
+
+
   def __init__(self, session):
     super(LDAPAuthBackend, self).__init__(session)
 
-  def auth(self, username, password):
-    if not super(LDAPAuthBackend, self).auth(username, password):
-      return False
-
-    ret = False
-
-    try:
-      ldapcon = LdapConnector(uid=username, password=password)
-      ret = True
-      self.initSession(username)
-    except:
-      pass
-
-    return ret
+  def __auth__(self, username, password):
+    LdapConnector(uid=username, password=password)
+    return True
